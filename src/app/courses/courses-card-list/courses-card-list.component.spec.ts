551e5bbbdb50efbebeec01faf66e14343f95ed74
josyAlem <yoseph.click@gmail.com>
2021-09-14 19:36:39 +0300

    added test for pure presentation component

---
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -0,0 +1,63 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { setupCourses } from "../common/setup-test-data";
+import { CoursesModule } from "../courses.module";
+import { CoursesCardListComponent } from "./courses-card-list.component";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+
+fdescribe("CoursesCardListComponent", () => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [CoursesModule],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+      });
+  });
+
+  it("should create the component", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should display the course list", () => {
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    //console.log(el.nativeElement.outerHTML);
+
+    const cards = el.queryAll(By.css(".course-card"));
+    expect(cards).toBeTruthy("Could not find cards");
+    expect(cards.length).toBe(12, "unexpected number of courses");
+  });
+
+  it("should display the first course", () => {
+    component.courses = setupCourses();
+    fixture.detectChanges();
+
+    const course = component.courses[0];
+    const card = el.query(By.css(".course-card:first-child")),
+      title = card.query(By.css("mat-card-title")),
+      content = card.query(By.css("mat-card-content p")),
+      image = card.query(By.css("img"));
+
+    expect(card).toBeTruthy("Could not find first card");
+    expect(title.nativeElement.textContent).toBe(
+      course.titles.description,
+      "Incorrect course card title"
+    );
+    expect(image.nativeElement.src).toBe(
+      course.iconUrl,
+      "Incorrect course card image source"
+    );
+    expect(content.nativeElement.textContent).toBe(
+      course.titles.longDescription,
+      "Incorrect course card content"
+    );
+  });
+});
